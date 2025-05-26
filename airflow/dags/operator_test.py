83057456ae81520e7822dadccd65ca5a31166e09
renataalineb <renataalinebv@gmail.com>
2025-05-26 00:23:58 -0300

    Cricação de conexão/ uso de task group

--- airflow/dags/operator_test.py
+++ airflow/dags/operator_test.py
@@ -1,8 +1,8 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-#from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 import boto3
 import os
@@ -21,14 +21,15 @@ def pegando_dados_do_banco(**context): #pegar o "contexto de execução da DAG" 
 
     context['ti'].xcom_push(key='parquet_path', value=file_path)
 
+
 def upload_parquet_to_minio(**context):
     # Recupera o caminho salvo no XCom
     file_path = context['ti'].xcom_pull(
         key='parquet_path',
-        task_ids='select_and_print'
+        task_ids='processamento_dados.fazer_busca'
     )
 
-    minio_endpoint = os.environ['MINIO_ENDPOINT']
+    minio_endpoint = os.environ.get('MINIO_ENDPOINT','http://localhost:9000')
     access_key = os.environ['MINIO_ACCESS_KEY']
     secret_key = os.environ['MINIO_SECRET_KEY']
     bucket_name = 'bucket-de-dados-da-atividade'
@@ -49,7 +50,7 @@ with DAG(
 
     dag_id= "operator_test", 
     start_date=datetime(2024, 1, 1), 
-    schedule=None, 
+    schedule_interval=None, 
     catchup=False
     ) as dag:
         
@@ -57,27 +58,29 @@ with DAG(
         task_id="usando_bash", 
         bash_command="echo 'Essa é a DAG de exemplo, aqui estão meus testes para aprendizado'"
     )
-    fazer_busca=PythonOperator(
-        task_id="fazer_busca",
-        python_callable=pegando_dados_do_banco,
-        provide_context=True
-    )
+    with TaskGroup("processamento_dados") as processamento:
+        fazer_busca = PythonOperator(
+            task_id="fazer_busca",
+            python_callable=pegando_dados_do_banco,
+            provide_context=True
+        )
 
-    """select_task= PostgresOperator
-    (
-      task_id="select_coluna",
-      postgres_conn_id="postgres_airflow",
-      sql="SELECT * FROM public.connection WHERE host = 'localhost';"
-    )"""
+        upload_task = PythonOperator(
+            task_id="upload_parquet_to_minio",
+            python_callable=upload_parquet_to_minio,
+            provide_context=True
+        )
 
-    upload_task = PythonOperator(
-        task_id="upload_parquet_to_minio",
-        python_callable=upload_parquet_to_minio,
-        provide_context=True
-    )
+        # select_task = PostgresOperator(
+        #     task_id="select_coluna",
+        #     postgres_conn_id="postgres_airflow",
+        #     sql="SELECT * FROM public.connection WHERE host = 'localhost';"
+        # )
 
+ # Ordem de execução das tarefas
+        fazer_busca >> upload_task
 
-    # Ordem de execução das tarefas
-    usando_bash >> fazer_busca >> upload_task
-    
+    usando_bash >> processamento
+
+   
 
